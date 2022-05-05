ce48f8acf28cdcc79e231f80375ad9a41e250662
Utkarsh <r.utkarsh.0010@gmail.com>
2022-05-06 02:09:16 +0530

    Peer connection established

--- server.js
+++ server.js
@@ -1,11 +1,25 @@
+// imports
 const express = require('express')
+const app     = express()
+const server  = require('http').Server(app)
+const io      = require('socket.io')(server)
+
+const { ExpressPeerServer } = require('peer')
+const peerServer            = ExpressPeerServer(server, {
+    debug: true,
+    proxied: true,
+}) 
+
 const {v4: uuidv4} = require('uuid')
-const app = express()
 
-const server = require('http').Server(app)
+var cors = require('cors')
 
+
+// server configurations
+app.use(cors())
 app.set('view engine', 'ejs')
 app.use(express.static("public"))
+app.use('/peerjs', peerServer)
 
 app.get('/', (req, res) => {
     res.redirect(`/${uuidv4()}`)
@@ -13,6 +27,13 @@ app.get('/', (req, res) => {
 
 app.get('/:room', (req, res) => {
     res.render('room', {roomId: req.params.room})
+})
+
+io.on('connection', (socket) =>{
+    socket.on('join-room', (roomId, userId, userName) =>{
+        socket.join(roomId)
+        socket.to(roomId).emit("user-connected", userId, userName)
+    })
 })
 
 server.listen(3030)
